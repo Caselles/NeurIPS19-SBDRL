4be445170d3f022dd9d7243c18d3e8c29e745a31
Hugo Caselles-Dupré <casellesdupre.hugo@gmail.com>
2019-09-11 15:39:03 +0200

    Update env.py

--- code/reproduction_exp_CCI_VAE/flatland/flat_game/env.py
+++ code/reproduction_exp_CCI_VAE/flatland/flat_game/env.py
@@ -322,20 +322,20 @@ class Env(object):
             return False
 
         fruit_collision_handler = self.space.add_collision_handler(
-            a=0,
-            b=2
+            collision_type_a=0,
+            collision_type_b=2
         )
         fruit_collision_handler.begin = begin_fruit_collision
 
         poison_collision_handler = self.space.add_collision_handler(
-            a=0,
-            b=3
+            collision_type_a=0,
+            collision_type_b=3
         )
         poison_collision_handler.begin = begin_poison_collision
 
         goal_collision_handler = self.space.add_collision_handler(
-            a=0,
-            b=4
+            collision_type_a=0,
+            collision_type_b=4
         )
         goal_collision_handler.begin = begin_goal_collision
 
